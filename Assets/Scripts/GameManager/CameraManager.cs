5c07ec5950a9cd8946d409c830997f387fb36393
Kchapuis <98804154+Kchapuis@users.noreply.github.com>
2023-12-06 17:51:07 +0100

    doorv2

--- Assets/Scripts/GameManager/CameraManager.cs
+++ Assets/Scripts/GameManager/CameraManager.cs
@@ -6,19 +6,21 @@ using UnityEngine;
 public class CameraManager : MonoBehaviour
 {
     public Room RoomTofocus;
+    private Vector3 _targetPosition;
+    public float speed;
     void Start()
     {
-        
+        _targetPosition = transform.position;
     }
     public void ChangeFocusRoom(Room newRoomTofocus)
     {
         RoomTofocus = newRoomTofocus;
-        transform.position = new Vector3(RoomTofocus.transform.position.x, RoomTofocus.transform.position.y, -10.0f);
-
+        
+        _targetPosition = new Vector3(RoomTofocus.transform.position.x, RoomTofocus.transform.position.y, -10.0f);
     }
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, _targetPosition, speed * Time.deltaTime);
     }
 }
--- Assets/Scripts/GameManager/Porte.cs
+++ Assets/Scripts/GameManager/Porte.cs
@@ -17,15 +17,20 @@ public class Porte : MonoBehaviour
     }
     void OnTriggerEnter2D(Collider2D col)
     {
-        test = Camera.main.GetComponent<CameraManager>();
-        ActualRoom = !ActualRoom;
-        if(ActualRoom)
+        if (col is BoxCollider2D)
         {
-            test.ChangeFocusRoom(RoomIN);
+            test = Camera.main.GetComponent<CameraManager>();
+            Debug.Log("test");
+            ActualRoom = !ActualRoom;
+            if(ActualRoom)
+            {
+                test.ChangeFocusRoom(RoomIN);
+            }
+            else
+            {
+                test.ChangeFocusRoom(RoomOUT);
+            }
         }
-        else
-        {
-            test.ChangeFocusRoom(RoomOUT);
-        }
+        
     }
 }
